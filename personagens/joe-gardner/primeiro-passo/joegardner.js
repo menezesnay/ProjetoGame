8c9b9a62ed909452a2019b1d07e236b434f760a5
Nayara M <menezesnaym@gmail.com>
2022-06-06 22:44:05 -0300

    linkando os botoes do html escolhas para js

---
+++ personagens/joe-gardner/primeiro-passo/joegardner.js
@@ -0,0 +1,17 @@
+function validacaoDeDados(){
+    var escolhaUmaOpcao = prompt('Tome uma decisão: opção 1 A alma foi roubada do Sr Bigodes ou 2 A alma foi roubada da 22?');
+    while(true){
+        if(escolhaUmaOpcao == 1 || escolhaUmaOpcao == 2){
+            break
+        } else{
+            alert("invalido. escolha entre a opção 1 ou 2!");
+            escolhaUmaOpcao = prompt("Tome uma decisao: opção 1 A alma foi roubada do Sr Bigodes ou 2 A alma foi roubada da 22?");
+        } 
+    } function proximaFase(){
+        if(escolhaUmaOpcao == '1'){
+            window.location.href = 'primeiropassojoe.html';
+        } else{
+            window.location.href= 'segundopassojoe.html';
+        }
+    }   proximaFase();
+}
