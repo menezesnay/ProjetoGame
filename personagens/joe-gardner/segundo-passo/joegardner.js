f82a41aa20ab5d54403c76b01e3df26588a8884e
Nayara M <menezesnaym@gmail.com>
2022-06-09 00:29:54 -0300

    estilização e configurações da pag do JOE

---
+++ personagens/joe-gardner/segundo-passo/joegardner.js
@@ -0,0 +1,17 @@
+function validacaoDeDados(){
+    var escolhaUmaOpcao = prompt('Tome uma decisão: opção 1 Caiu no Metrô ou 2 Caiu no bueiro?');
+    while(true){
+        if(escolhaUmaOpcao == 1 || escolhaUmaOpcao == 2){
+            break
+        } else{
+            alert("invalido. escolha entre a opção 1 ou 2!");
+            escolhaUmaOpcao = prompt("Tome uma decisao: opcao 1 Caiu no Metrô ou 2 Caiu no bueiro?");
+        } 
+    } function proximaFase(){
+        if(escolhaUmaOpcao == '1'){
+            window.location.href = 'gameoverjoegardner.html';
+        } else{
+            window.location.href= 'terceiropassojoe.html';
+        }
+    }   proximaFase();
+}
